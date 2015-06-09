1b1556281dd9977a064da8455dd148230dd02feb
xieanhui <xieanhui@aliyun.com>
2015-06-09 20:29:44 +0800

    处理WINDOW打开多个网页在IE中死机现象

--- Js/eventUtil.js
+++ Js/eventUtil.js
@@ -1,35 +1,32 @@
-define(function () {
-   
-    function preventDefault(){
-        var ev = event || window.event;
-        if(ev && ev.preventDefault){
+define(function() {
+
+    function preventDefault(ev) {
+        if (ev && ev.preventDefault) {
             ev.preventDefault();
-        }else{
+        } else {
             ev.returnValue = false;
         }
     }
 
-    function stopPropagation(){
-        var ev = event || window.event;
-        var tg = ev.srcElement || ev.target;
-        if(ev && ev.stopPropagation){
+    function stopPropagation(ev) {
+        if (ev && ev.stopPropagation) {
             ev.stopPropagation();
-        }else {
+        } else {
             ev.cancelBubble = true;
         }
     }
 
-    function addHandler(args, sEvent, handler) {//给对象添加事件
+    function addHandler(args, sEvent, handler) { //给对象添加事件
         if (args.addEventListener) {
             args.addEventListener(sEvent, handler, false);
         } else if (args.attachEvent) {
             args.attachEvent("on" + sEvent, handler);
         } else {
-            args['on'+sEvent] = handler;
+            args['on' + sEvent] = handler;
         }
     }
 
-    function removeHandler(args, sEvent, handler) {//移除对象事件
+    function removeHandler(args, sEvent, handler) { //移除对象事件
         if (args.removeEventListener) {
             args.removeEventListener(sEvent, handler, false);
         } else if (args.detachEvent) {
@@ -37,36 +34,36 @@ define(function () {
         }
     }
 
-    function addLoadEvent(func) {//添加文档ONLOAD事件处理函数
+    function addLoadEvent(func) { //添加文档ONLOAD事件处理函数
         var oldLoad = window.onload;
         if (typeof window.onload != "function") {
             window.onload = func;
         } else {
-            window.onload = function () {
+            window.onload = function() {
                 oldLoad();
                 func();
             }
         }
     }
 
-    function addScrollEvent(func) {//添加滚动条事件处理函数
+    function addScrollEvent(func) { //添加滚动条事件处理函数
         var oldScroll = window.onscroll;
         if (typeof window.onscroll != "function") {
             window.onscroll = func;
         } else {
-            window.onscroll = function () {
+            window.onscroll = function() {
                 oldScroll();
                 func();
             }
         }
     }
 
-    function addResizeEvent(func) {//添加滚动条事件处理函数
+    function addResizeEvent(func) { //添加滚动条事件处理函数
         var oldResize = window.onresize;
         if (typeof window.onresize != "function") {
             window.onresize = func;
         } else {
-            window.onresize = function () {
+            window.onresize = function() {
                 oldResize();
                 func();
             }
